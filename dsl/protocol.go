e0f14e5c9ae0bf195bf1e4c526b8ac25894fd26c
longan55 <17673481176@162.com>
2025-06-12 23:28:58 +0800

    0612

--- dsl/protocol.go
+++ dsl/protocol.go
@@ -9,9 +9,46 @@ type Start struct {
 	Value []byte
 }
 type DataLength struct {
+	Index      int
+	SelfLength int    //表示"长度"的数据长度不超过2字节
+	Encoding   string //BCD、BIN、ASCII
+	Endian     int    //0小端,1大端
+}
+
+// SerialNumber 默认2字节
+type SerialNumber struct {
 	Index    int
-	Length   int
-	Encoding string //BCD、BIN、ASCII
+	Encoding string
+	Endian   int //0小端,1大端
+}
+
+// Encryption 加密标志
+type Encryption struct {
+	Index     int
+	Algorithm string
+	Range     [2]int //范围,起始索引 - 终止索引
+}
+
+// DataType 数据类型
+type DataType struct {
+	Index      int
+	SelfLength int //一般1字节
+	Encoding   string
+	Endian     int //0小端,1大端
+}
+
+// DataDomain 数据域
+type DataDomain struct {
+	Index int
+}
+
+// CheckSum 校验码
+type CheckSum struct {
+	Index      int
+	SelfLength int //一般1字节
+	Algorithm  string
+	Encoding   string
+	Endian     int //0小端,1大端
 }
 
 /*
