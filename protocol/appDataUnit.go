f0c36cd536ec7cc291b424f0d113ba9c14f4867e
longan55 <17673481176@163.com>
2025-06-26 22:46:03 +0800

    refactor(protocol): 将数据打包和应用数据单元模块移动到protocol包
    重构代码结构，将datapack.go和appDataUnit.go从main包移动到protocol包，以更好地组织代码结构并提高模块化程度

--- protocol/appDataUnit.go
+++ protocol/appDataUnit.go
@@ -1,4 +1,4 @@
-package main
+package protocol
 
 import (
 	"bytes"
@@ -7,8 +7,6 @@ import (
 	"fmt"
 	"io"
 	"reflect"
-
-	"github.com/longan55/proto/protocol"
 )
 
 //通过框架配置协议，框架自动解析和封装，无需自己开发。
@@ -46,7 +44,7 @@ func (adu *Adu) Debug(r io.Reader, source []byte) {
 	// 数据域长度 要传给数据域元素作为长度
 	// 加密标志 是否对指定元素的值进行加密或解密
 	// 校验码 是否对指定元素进行校验计算
-	offset := uint16(0)
+	offset := 0
 	//遍历所有元素
 	for _, field := range adu.Fields {
 		//根据元素Field获取对应数据切片
@@ -54,10 +52,10 @@ func (adu *Adu) Debug(r io.Reader, source []byte) {
 		//更新偏移量
 		offset += field.Length()
 		//debug打印元素
-		if field.Scale() == 0 {
-			fmt.Printf("[%s] = %0d", field.Name(), data)
+		if field.GetScale() == 0 {
+			fmt.Printf("[%s] = %0d", field.GetName(), data)
 		} else {
-			fmt.Printf("[%s] = %0x", field.Name(), data)
+			fmt.Printf("[%s] = %0x", field.GetName(), data)
 		}
 
 		//处理方法
@@ -71,34 +69,47 @@ func (adu *Adu) Debug(r io.Reader, source []byte) {
 // Fielder 元素接口
 type Fielder interface {
 	//获取元素名称
-	Name() string
-	// Value 获取默认值
-	Value() []byte
-	// Scale 获取进制
-	Scale() uint8
+	GetName() string
+	SetName(name string)
+	SetDefault(value []byte)
+	GetDefault() []byte
+	//获取实际值
+	RealValue() []byte
 	// SetLen 设置元素长度
-	SetLen(uint16)
+	SetLen(int)
 	// Length 获取元素长度
-	Length() uint16
+	Length() int
+	SetScale(uint8)
+	//获取进制
+	GetScale() uint8
+
+	// SetRange 设置范围
+	SetRange(start, end uint8)
+	// GetRange 获取范围
+	GetRange() (start, end uint8)
 	// Deal 解析元素
 	Deal([]byte) error
 }
 
 type fieldType byte
 
+var _ Fielder = (*Field)(nil)
+
 // Field 基础元素结构体
 type Field struct {
-	Fielder
 	name string //消息帧 元素名字
 	//FType        fieldType      //消息帧 字段类型
-	scale        uint8            // 1十六进制，0十进制
-	Len          uint16           //消息帧 元素本身长度
-	DefaultValue []byte           //默认值
-	Order        binary.ByteOrder //大小端
-	Next         *Fielder
+	scale    uint8            // 1十六进制，0十进制
+	len      int              //消息帧 元素本身长度
+	defaultV []byte           //默认值
+	order    binary.ByteOrder //大小端
+	next     *Fielder
+	start    uint8
+	end      uint8
+	DealFunc func(field Fielder, data []byte) error
 }
 
-func (f *Field) Name() string {
+func (f *Field) GetName() string {
 	return f.name
 }
 
@@ -106,29 +117,64 @@ func (f *Field) SetName(name string) {
 	f.name = name
 }
 
-func (f *Field) SetLen(l uint16) {
-	f.Len = l
-}
-func (f *Field) Length() uint16 {
-	return f.Len
+func (f *Field) SetDefault(val []byte) {
+	f.defaultV = val
 }
 
-func (f *Field) Scale() uint8 {
-	return f.scale
+func (f *Field) GetDefault() []byte {
+	return f.defaultV
+}
+
+func (f *Field) RealValue() []byte {
+	return f.defaultV
+}
+func (f *Field) SetLen(l int) {
+	f.len = l
+}
+func (f *Field) Length() int {
+	return f.len
 }
 
 func (f *Field) SetScale(u uint8) {
 	f.scale = u
 }
 
-func (f *Field) SetDefaultValue(val []byte) {
-	f.DefaultValue = val
+func (f *Field) GetScale() uint8 {
+	return f.scale
 }
-func (f *Field) SetDefaultValueString(str string) {
-	f.DefaultValue = []byte(str)
+
+func (f *Field) GetRange() (start, end uint8) {
+	return f.start, f.end
 }
-func (f *Field) Value() []byte {
-	return f.DefaultValue
+func (f *Field) SetRange(start, end uint8) {
+	f.start = start
+	f.end = end
+}
+
+func (f *Field) Deal(data []byte) error {
+	return f.DealFunc(f, data)
+}
+
+// 起始符
+func NewStarter(start []byte) Fielder {
+	field := &Field{
+		name:     "起始符",
+		defaultV: start,
+		len:      len(start),
+	}
+	field.DealFunc = func(field Fielder, data []byte) error {
+		if data == nil {
+			return errors.New("数据为空")
+		}
+		if len(data) < field.Length() {
+			return errors.New("数据长度小于起始符长度")
+		}
+		if bytes.Equal(data[:field.Length()], field.GetDefault()) {
+			return fmt.Errorf("起始符错误Need:%s,But:%s", string(field.GetDefault()), string(data[:field.Length()]))
+		}
+		return nil
+	}
+	return field
 }
 
 type Starter struct {
@@ -136,12 +182,12 @@ type Starter struct {
 }
 
 func (start Starter) Deal(data []byte) error {
-	if len(data) != int(start.Len) {
+	if len(data) != int(start.len) {
 		return errors.New("起始长度不对")
 	}
-	for i, v := range start.DefaultValue {
+	for i, v := range start.defaultV {
 		if v != data[i] {
-			fmt.Printf("起始：%# 02x，预期：%# 02x\n", data, start.DefaultValue)
+			fmt.Printf("起始：%# 02x，预期：%# 02x\n", data, start.defaultV)
 			return errors.New("起始值错误")
 		}
 	}
@@ -154,18 +200,18 @@ type DataLen struct {
 }
 
 func (d DataLen) Deal(data []byte) error {
-	if len(data) != int(d.Len) {
+	if len(data) != int(d.len) {
 		return errors.New("数据域长度字段本省身长度不对")
 	}
-	var l = make([]byte, d.Len)
-	data = data[:d.Len]
+	var l = make([]byte, d.len)
+	data = data[:d.len]
 	buffer := bytes.NewBuffer(data)
-	err := binary.Read(buffer, d.Order, l)
+	err := binary.Read(buffer, d.order, l)
 	if err != nil {
 		fmt.Println("binary read error", err)
 		return err
 	}
-	bin2Uint64, err := protocol.BIN2Uint64(data, d.Order)
+	bin2Uint64, err := BIN2Uint64(data, d.order)
 	if err != nil {
 		fmt.Println("b2i error: ", err)
 		return err
--- protocol/datapack.go
+++ protocol/datapack.go
@@ -1,4 +1,4 @@
-package main
+package protocol
 
 type DataPacker interface {
 	Pack() []byte
