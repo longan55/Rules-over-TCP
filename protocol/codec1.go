4068ca249c8517aaa9bb5384974dcf73b0d8ea05
longan55 <17673481176@163.com>
2025-10-16 23:29:20 +0800

    251016

--- protocol/codec1.go
+++ protocol/codec1.go
@@ -1,6 +1,42 @@
 package protocol
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+)
+
+type EncodeBuilder struct {
+	order binary.ByteOrder
+}
+
+func NewEncoder(order binary.ByteOrder) *EncodeBuilder {
+	return &EncodeBuilder{order: order}
+}
+
+func (builder *EncodeBuilder) BIN() *EncodeBIN {
+	return &EncodeBIN{order: builder.order}
+}
+
+type EncodeBIN struct {
+	order binary.ByteOrder
+}
+
+func (bin EncodeBIN) Integer() DataTypeInteger {
+	return DataTypeInteger{}
+}
+
+type DataTypeInteger struct{}
+
+func (i DataTypeInteger) Integer() int {
+	return 0
+}
+
+type BCD struct {
+	order binary.ByteOrder
+}
+
+type ASCII struct {
+	order binary.ByteOrder
+}
 
 type CodeItem struct {
 	Order    binary.ByteOrder
@@ -29,24 +65,11 @@ func (c *CodeItem) SetEnum(enum map[any]any) {
 func (c *CodeItem) Extract() any {
 	switch c.Encode {
 	case EncodingBIN:
-		switch c.Datatype {
-		case DataTypeInt:
-			uint64, err := BIN2Uint64(nil, c.Order)
-			if err != nil {
-				return nil
-			}
-			return int64(uint64)
-		case DataTypeFloat:
-			float64, err := Bin2Float64(c.Order, nil, 6)
-			if err != nil {
-				return nil
-			}
-			return float64
-		case DataTypeString:
-			panic("EncodingType BIN is not support DataTypeString")
-		default:
+		value, err := decode(c.Order, c.Datatype)
+		if err != nil {
 			return nil
 		}
+		return value
 	case EncodingBCD:
 		switch c.Datatype {
 		case DataTypeInt:
@@ -62,5 +85,26 @@ func (c *CodeItem) Extract() any {
 		return c.Enum
 	default:
 		return nil
+	}
+}
+
+func decode(order binary.ByteOrder, datatype DataType) (any, error) {
+	switch datatype {
+	case DataTypeInt:
+		uint64, err := BIN2Uint64(nil, order)
+		if err != nil {
+			return nil, err
+		}
+		return int64(uint64), nil
+	case DataTypeFloat:
+		float64, err := Bin2Float64(order, nil, 6)
+		if err != nil {
+			return nil, err
+		}
+		return float64, nil
+	case DataTypeString:
+		panic("EncodingType BIN is not support DataTypeString")
+	default:
+		return nil, nil
 	}
 }
