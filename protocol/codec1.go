6640c033e0d50fded4c51e4bef28160b26986ee8
longan55 <17673481176@163.com>
2025-10-17 18:28:22 +0800

    251017

--- protocol/codec1.go
+++ protocol/codec1.go
@@ -4,6 +4,11 @@ import (
 	"encoding/binary"
 )
 
+type Encoder interface {
+	// 如果希望 value 是传地址/指针，入参类型应为指针类型，使用 *any 表示任意类型的指针
+	encode(data []byte, value any) error
+}
+
 type EncodeBuilder struct {
 	order binary.ByteOrder
 }
@@ -17,18 +22,145 @@ func (builder *EncodeBuilder) BIN() *EncodeBIN {
 }
 
 type EncodeBIN struct {
-	order binary.ByteOrder
+	byteLength int
+	order      binary.ByteOrder
 }
 
-func (bin EncodeBIN) Integer() DataTypeInteger {
-	return DataTypeInteger{}
+func (bin *EncodeBIN) SetByteLength(byteLength int) *EncodeBIN {
+	bin.byteLength = byteLength
+	return bin
 }
 
-type DataTypeInteger struct{}
-
-func (i DataTypeInteger) Integer() int {
-	return 0
+func (bin *EncodeBIN) encode(data []byte, value any) error {
+	return nil
 }
+
+func (bin *EncodeBIN) Integer() *DataTypeInteger {
+	return &DataTypeInteger{
+		encoder: bin,
+		order:   bin.order,
+		mul:     1,
+	}
+}
+
+func (bin *EncodeBIN) Float1() *DataTypeFloat1 {
+	return &DataTypeFloat1{
+		order: bin.order,
+		mul:   1,
+	}
+}
+
+type DataTypeInteger struct {
+	encoder  Encoder
+	order    binary.ByteOrder
+	mflag    bool
+	oflag    bool
+	mul      float64
+	offset   float64
+	srcValue int
+	enum     map[int]any
+	bitmap   map[int]any
+}
+
+// TODO:Multiple和Offset，考虑是否需要设置幂等性
+func (i *DataTypeInteger) Multiple(mul float64) *DataTypeInteger {
+	i.mul = mul
+	if !i.oflag {
+		i.mflag = true
+	}
+	return i
+}
+
+// Offset: 设置偏移量，注意和Multiple的先后顺序，将影响计算顺序
+func (i *DataTypeInteger) Offset(offset float64) *DataTypeInteger {
+	i.offset = offset
+	if i.mflag {
+		i.oflag = true
+	}
+	return i
+}
+
+func (i *DataTypeInteger) SetEnum(enum map[int]any) *DataTypeInteger {
+	i.enum = enum
+	return i
+}
+
+func (i *DataTypeInteger) SetBitMap(bitmap map[int]any) *DataTypeInteger {
+	i.bitmap = bitmap
+	return i
+}
+
+func (i *DataTypeInteger) SourceValue() int {
+	i.encoder.encode(nil, &i.srcValue)
+	if i.mflag {
+		i.offset = i.offset * i.mul
+		return i.srcValue*int(i.mul) + int(i.offset)
+	} else if i.oflag {
+		i.offset = i.offset + i.offset
+		return (i.srcValue + int(i.offset)) * int(i.mul)
+	} else {
+		return i.srcValue
+	}
+}
+
+func (i *DataTypeInteger) ExplainedValue() any {
+	if i.enum == nil && i.bitmap == nil {
+		return i.SourceValue()
+	}
+	if i.enum != nil {
+		return i.enum[i.SourceValue()]
+	} else {
+		//i.bitmap != nil
+		result := make([]string, 0, len(i.bitmap))
+		source := i.SourceValue()
+		for k, v := range i.bitmap {
+			if (source & (1 << k)) != 0 {
+				result = append(result, v.(string))
+			}
+		}
+		return result
+	}
+}
+
+type DataTypeFloat1 struct {
+	order    binary.ByteOrder
+	mflag    bool
+	oflag    bool
+	mul      float64
+	offset   float64
+	srcValue float64
+}
+
+func (f *DataTypeFloat1) Multiple(mul float64) {
+	f.mul = mul
+	if !f.oflag {
+		f.mflag = true
+	}
+}
+
+func (f *DataTypeFloat1) Offset(offset float64) {
+	f.offset = offset
+	if f.mflag {
+		f.oflag = true
+	}
+}
+
+func (f *DataTypeFloat1) SourceValue() float64 {
+	f.srcValue = float64(Bin2Int(nil, f.order))
+	if f.mflag {
+		return f.srcValue*f.mul + f.offset
+	} else if f.oflag {
+		return (f.srcValue + f.offset) * f.mul
+	} else {
+		return f.srcValue
+	}
+}
+
+func (f *DataTypeFloat1) ExplainedValue() float64 {
+	return f.SourceValue()
+}
+
+type DataTypeString1 struct{}
 
 type BCD struct {
 	order binary.ByteOrder
--- protocol/codec1_test.go
+++ protocol/codec1_test.go
@@ -1,9 +1,12 @@
 package protocol
 
 import (
+	"encoding/binary"
 	"testing"
 )
 
 func TestCode1(t *testing.T) {
-
+	encoder := NewEncoder(binary.LittleEndian)
+	i := encoder.BIN().SetByteLength(4).Integer().Multiple(2).Offset(100)
+	i.SourceValue()
 }
